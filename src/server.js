2bd597adfad2a72af698d5121b7305a9930c2cc7
kuro2904 <trung2001sgp@gmail.com>
2025-10-06 16:15:37 +0700

    [FIX] Call API outside of handler

--- src/server.js
+++ src/server.js
@@ -2,14 +2,25 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://vietvibe.org/api/";
 
-const res = await axios.post("/webhooks/notify", {
-  data: {},
-  headers:{
-    'Content-Type': 'application/json',
-  }
-});
+export default {
+  async fetch() {
+    try {
+      const res = await axios.post("/webhooks/notify", {}, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-console.log(res.data);
+      console.log("Response:", res.data);
 
-
-export default axios;
+      return new Response(JSON.stringify(res.data), {
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      return new Response(
+        JSON.stringify({ error: err.message }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
+      );
+    }
+  },
+};
