4c7013caa70d0ae4bbfee934b5e149301cf66867
kuro2904 <trung2001sgp@gmail.com>
2025-10-06 15:44:33 +0700

    [FEATURE] Ping to Main-web to push notification

--- src/server.js
+++ src/server.js
@@ -1,20 +1,15 @@
 import axios from "axios";
 
-const SECRET_TRUST_CLIENT = process.env.SECRET_TRUST_CLIENT;
+axios.defaults.baseURL = "https://vietvibe.org/api/";
 
-// axios.defaults.baseURL = "https://vietvibe.org/api/";
+const res = await axios.post("/webhooks/notify", {
+  data: {},
+  headers:{
+    'Content-Type': 'application/json',
+  }
+});
 
-// const res = await axios.post("/webhooks/notify", {
-//   data: {},
-//   headers:{
-//     'Content-Type': 'application/json',
-//     'secret': '1D1683DF674794D34E6CB956322FC'
-//   }
-// });
+console.log(res.data);
 
-// console.log(res.data);
-
-
-console.log("SECRET_TRUST_CLIENT", SECRET_TRUST_CLIENT);
 
 export default axios;
